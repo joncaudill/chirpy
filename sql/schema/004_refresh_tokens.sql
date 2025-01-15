f125fe9643eec793f645915ec2f05a4d55fa74fc
joncaudill <jon.caudill@outlook.com>
2025-01-15 14:03:02 -0500

    add migration for refresh_tokens table

--- sql/schema/004_refresh_tokens.sql
+++ sql/schema/004_refresh_tokens.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE refresh_tokens (
-  token string PRIMARY KEY,
+  token TEXT PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
   user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
